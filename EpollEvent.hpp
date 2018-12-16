3cda5b46859ff64325196ae3a61511c377f283fe
ReimuNotMoe <34613827+ReimuNotMoe@users.noreply.github.com>
2018-12-16 22:12:16 +0800

    Add template for integers

--- EpollEvent.hpp
+++ EpollEvent.hpp
@@ -27,11 +27,6 @@ namespace EpollPlus {
     public:
 	EpollEvent() = default;
 
-//	explicit EpollEvent(const epoll_event &__event) {
-//		raw_event = new epoll_event;
-//		memcpy(raw_event, &__event, sizeof(epoll_event));
-//	}
-
 	EpollEvent(const uint32_t __events, const std::shared_ptr<RawMetaData> &__sptr_rawmetadata) {
 		events = __events;
 		sptr_raw_metadata = __sptr_rawmetadata;
@@ -119,44 +114,102 @@ namespace EpollPlus {
     };
 
 
-//    template<class T>
-//    class EpollEvent<T, typename std::enable_if<is_int<T>::value>::type> {
-//    private:
-//	epoll_event *raw_event = nullptr;
-//	bool is_existing_raw_metadata = false;
-//	std::shared_ptr<epoll_event *> sptr_raw_events_array;
-//	size_t pos_rawevents = 0;
-//    public:
-//	EpollEvent() = default;
-//
-//	explicit EpollEvent(const epoll_event &__event) {
-//		raw_event = new epoll_event;
-//	}
-//
-//	EpollEvent(const std::shared_ptr<epoll_event *> &__events_sptr, size_t __pos) {
-//		sptr_raw_events_array = __events_sptr;
-//		pos_rawevents = __pos;
-//		raw_event = *sptr_raw_events_array + sizeof(epoll_event) * __pos;
-//	}
-//
-//	EpollEvent(T user_data, uint32_t events) {
-//		raw_event = new epoll_event;
-//		memcpy(&raw_event->data.u64, &user_data, sizeof(T));
-//		raw_event->events = events;
-//	}
-//
-//	~EpollEvent() {
-//		if (raw_event) {
-//			if (!is_existing_raw_metadata)
-//				delete raw_event;
-//		}
-//	}
-//
-//	void aaa() {
-//		printf("Using class ints\n");
-//	}
-//
-//
-//    };
+    template<class T>
+    class EpollEvent<T, typename std::enable_if<is_int<T>::value>::type> {
+    private:
+	int fd = -1;
+	uint32_t events = 0;
+	std::shared_ptr<RawMetaData> sptr_raw_metadata;
+
+    public:
+	EpollEvent() = default;
+
+	EpollEvent(const uint32_t __events, const std::shared_ptr<RawMetaData> &__sptr_rawmetadata) {
+		events = __events;
+		sptr_raw_metadata = __sptr_rawmetadata;
+		fd = (*sptr_raw_metadata).fd;
+	}
+
+	EpollEvent(int user_fd, T user_data, uint32_t __events) {
+		fd = user_fd;
+		events = __events;
+
+		sptr_raw_metadata = std::make_shared<RawMetaData>();
+		auto raw_metadata = sptr_raw_metadata.get();
+
+		raw_metadata->fd = fd;
+		memcpy(&(raw_metadata->data.number), &user_data, sizeof(T));
+	}
+
+	T const UserData() const {
+		T ret;
+		memcpy(&ret, &(*sptr_raw_metadata).data.number, sizeof(T));
+		return ret;
+	}
+
+	EpollEvent &SetUserData(T val) {
+		memcpy(&(*sptr_raw_metadata).data.number, &val, sizeof(T));
+		return *this;
+	}
+
+	int FD() const {
+		return fd;
+	}
+
+	EpollEvent &SetFD(int __fd) {
+		fd = __fd;
+		return *this;
+	}
+
+	const std::shared_ptr<RawMetaData> &RawUserData () const {
+		return sptr_raw_metadata;
+	}
+
+	std::shared_ptr<epoll_event> RawEvent() const {
+		auto ret = std::make_shared<epoll_event>();
+
+		(*ret).events = events;
+		(*ret).data.ptr = sptr_raw_metadata.get();
+
+		return ret;
+	};
+
+	const uint32_t Events() {
+		return events;
+	}
+
+	EpollEvent &SetEvents(uint32_t __events) {
+		events = __events;
+		return *this;
+	}
+
+	uint32_t operator& (uint32_t rhs) {
+		return events & rhs;
+	}
+
+	uint32_t operator| (uint32_t rhs) {
+		return events | rhs;
+	}
+
+	uint32_t operator&= (uint32_t rhs) {
+		return (events &= rhs);
+	}
+
+	uint32_t operator|= (uint32_t rhs) {
+		return (events |= rhs);
+	}
+
+	bool operator== (const EpollEvent& rhs) {
+		return (events == rhs.events) && (sptr_raw_metadata == rhs.sptr_raw_metadata);
+	}
+
+	bool operator!= (const EpollEvent& rhs) {
+		return (events != rhs.events) || (sptr_raw_metadata != rhs.sptr_raw_metadata);
+	}
+
+	void aaa() {
+		printf("Using class integer\n");
+	}
+    };
 }
 #endif //LIBEPOLLPLUS_EPOLLEVENT_HPP
--- test.cpp
+++ test.cpp
@@ -13,22 +13,31 @@ int main() {
 	EpollEvent<std::string> ev0(STDIN_FILENO, &mystr, EPOLLIN);
 
 
-	myep.Add(ev0.SetFD(1));
+	myep.Add(ev0.SetFD(0).SetUserData(&mystr));
 
 	int i=10;
 
 	int flag = fcntl(0, F_GETFL) | O_NONBLOCK;
 	fcntl(0, F_SETFL, flag);
 
-	while (1) {
+
+	EpollEvent<std::string> thisep0;
+
+	while (i--) {
 		for (auto &ev: myep.Wait()) {
 			if (ev & EPOLLIN)
 			printf("fd=%d, events=%d, data=%p %s\n", ev.FD(), ev.Events(), ev.UserData(),
 			       ev.UserData()->c_str());
 
+			thisep0 = ev;
 			read(STDIN_FILENO, buf, 32768);
 		}
 	}
+
+
+	ev0 |= EPOLLOUT;
+	myep.Modify(ev0);
+	myep.Delete(thisep0);
 
 	return 0;
 }
